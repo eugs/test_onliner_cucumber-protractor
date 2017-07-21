a1badf0592cdb31ac9eaa0ccb0ca2e78189f8e40
Eugen Stotik <eugen_stotik@epam.com>
2017-07-21 13:44:48 +0300

    Clean trash

--- features/documentation.feature
+++ features/documentation.feature
@@ -2,26 +2,20 @@
 Feature: Example feature
   As a user of Onliner.by
   I want to see Catalog
-  So that I can find mobile phones
-
-#  Scenario: Reading documentation
-#    Given I am on the Onliner page
-#    When I click on "Каталог"
-#    Then I should see "Running specific features"
+  So that I can sure that iPhone 6s is better than iPhone SE
 
 Scenario: Searching for iPhones
     Given I am on the Onliner page
     Then I should see the title "Onliner.by"
 
+    #----------------------------
     When I click on "Каталог"
     When I click on "Мобильные телефоны"
     Then I should see the title "Мобильный телефон купить в Минске"
     When I choose "Apple" checkbox
     Then I should see the tag "Apple"
-
       When I find and click "Apple iPhone 6s 16GB Silver"
       Then I should see the title "Смартфон Apple iPhone 6s 16GB Silver купить в Минске"
-
     When I add to comparison
     Then notification panel should say "1 товар в сравнении"
 
@@ -31,39 +25,14 @@ Scenario: Searching for iPhones
     Then I should see the title "Мобильный телефон купить в Минске"
     When I choose "Apple" checkbox
     Then I should see the tag "Apple"
-
       When I find and click "Apple iPhone SE 16GB Space Gray"
       Then I should see the title "Смартфон Apple iPhone SE 16GB Space Gray купить в Минске"
-
     When I add to comparison
     Then notification panel should say "2 товара в сравнении"
 
+    #----------------------
     When I check the comparison page
     Then I should see the title "Сравнить Apple iPhone 6s 16GB Silver, Apple iPhone SE 16GB Space Gray"
     Then I compare first is better
-
-    #----------------------
-
     When I delete comparisons
     Then I should see the title "Каталог Onliner.by"
-
-
-
-  #  When I choose "32 ГБ"
-  #  Then I should see the tag "32 ГБ"
-
-  #  When I searching for "iPhone 7 16 GB"
-  #  Then I should see "Производитель"
-  #  When I click on "Apple"
-  #  Then I should see tag "apple"
-  #  When I choose for comparison model "Apple iPhone 6"
-  #  Then I should see panel "1 товар в сравнении"
-  #  When I choose for comparison model "Apple iPhone SE"
-  #  Then I should see panel "2 товара в сравнении"
-
-
-
-  #  When I click on "Каталог"
-  #  Then I should see "Каталог"
-  #  When I click phones
-  #  Then I should see the title "Мобильный телефон купить в Минске"
--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -40,7 +40,6 @@ defineSupportCode(function({Given, When, Then}) {
     del_btn.click()
     .then(()=> {
       return console.log("deleted");
-      //TODO assert
     })
   });
 
@@ -51,23 +50,22 @@ defineSupportCode(function({Given, When, Then}) {
     })
   });
 
-  //TODO refactor
   Then('I compare first is better', function () {
     var first_loc = 'td:nth-child(3).product-table__cell.product-table__cell_accent';
     var second_loc = 'td:nth-child(4).product-table__cell.product-table__cell_accent';
     var first = 0;
     var second = 0;
 
-		this.browser.$$(first_loc).count().then(res => {
+		this.browser.$$(first_loc).count().then((res) => {
 			first = res;
 		})
 		.then(() => {
-			this.browser.$$(second_loc).count().then(res => {
+			this.browser.$$(second_loc).count().then((res) => {
 				second = res;
 			})
 		})
 		.then(() => {
-			return assert.isAbove(first, second, 'something is wrong');
+			return assert.isAbove(first, second, 'something is wrong, the first phone should be better!');
 		});
 	});
 
@@ -100,196 +98,5 @@ defineSupportCode(function({Given, When, Then}) {
     //TODO add tag search
     return this.browser.wait(condition, 5000);
   });
-
-  // Then('I should see {stringInDoubleQuotes}', function (text) {
-  //   var xpath = "//*[contains(text(),'" + text + "')]";
-  //   var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-  //   return this.driver.wait(condition, 5000);
-  // });
-
-  // When('I searching for {stringInDoubleQuotes}', function (query) {
-  //   var element = this.browser.element(by.css('.fast-search__input'));
-  //   element.sendKeys(query);
-  //
-  //   var css_loc = ".result__item.result__item_product";
-  //   var condition = EC.presenceOf(this.browser.element(by.css(css_loc)));
-  //   return this.browser.wait(condition, 10000);
-  // });
-
-//   When('I setting up "{category}" with "{checkbox}"', function (category, checkboxName) {
-//     // console.log("setting up ", category, " with: ", checkboxName);
-//     // var fields = this.browser.element.all(by.css('.schema-filter__fieldset'))
-//     var css_loc = '.schema-filter__fieldset .schema-filter__facet span[class="schema-filter__checkbox-text"]';
-//
-// //     var checks = this.browser.$$(css_loc)
-// //       .then(function (ar) {
-// //         console.log("FIND: ", ar.length);
-// //       })
-// //
-// //     // var checks = this.browser.$$(css_loc)
-// //       .filter(function(element, index) {
-// //         element.getText().then(function(text) {
-// //           console.log("TEFSDF HERE : ", "'" +text + "'");
-// //             console.log("compars : ", category, (text  === checkboxName));
-// //             return (text  === checkboxName);
-// //       });
-// //     });
-//
-// // this.browser.$$(css_loc).filter(function(elem, index) {
-// //   return elem.getText().then(function(text) {
-// //     // console.log("TEFSDF HERE : ", "'" +text + "'");
-// //       // console.log("compars : ", checkboxName, (text  === checkboxName));
-// //     return text === checkboxName;
-// //   });
-// // }) .then(function (ar) {
-// //     // ar[0].click();
-// //     ar.get(0).click()
-// //       .then(function () {
-// //         console.log("CLICKED ");
-// //         this.browser.sleep(3000);
-// //       })
-// //     // console.log("FIND: ", .));
-// // })
-// //   // checkbox.isEnabled()
-//   //   .then(function (txt) {
-//   //     console.log("NAME: ", txt);
-//   //   })
-//
-//
-// })
-      // find manufacturer
-      // var field = this.browser.$$('.schema-filter__label span')
-
-//       { var field = this.browser.$$('.schema-filter__fieldset')
-//         .$$('.schema-filter__label span')
-//           // var categories = field.$$('.schema-filter__label span')
-//         .filter(function(element, index) {
-//             element.getText().then(function(text) {
-//                     // console.log("TEFSDF HERE : ", "'" +text + "'");
-//                     // console.log("compars : ", category, (text  === category));
-//                   return (text  === category);
-//                 });
-//       // }).then(function () {
-//       //   console.log("CATS: ", categories.length);
-//       //
-//       // })
-//     }).first();
-//
-//     // find Apple
-//     var model = field.all(by.css('span[class="schema-filter__checkbox-text"]'))
-//         .then(function (elements) {
-//           console.log("found models ", elements.length);
-//         })
-//
-//     // .filter(function(element, index) {
-//     //   return element.getText().then(function(text) {
-//     //     return text === checkboxName;
-//     //   });
-//     // }).first().click();
-//
-//         // field.all(by.css(''))
-//         //   .then(function (model) {
-//         //     console.log("model : ", model);
-//         //   });
-//
-//       // find Apple
-//         // field.all(by.css('.schema-filter__checkbox-text'))
-//
-//
-//
-//
-//       // })
-//       // .then(function (field) {
-//       //   console.log("FIELD ", field.length);
-//       //
-//       // })
-//
-//
-// // list of models
-// // .schema-filter__list
-//           //
-//           // field.getSize()
-//           //   .then(function (size) {
-//           //     console.log("SIFSDF ", size);
-//           //   })
-//  }
-
-
-
-  // })
-
-  // When('OLD I searching for {stringInDoubleQuotes}', function (text) {
-  //   // this.browser.element(by.css('.fast-search__input')).getWebElement().sendKeys(text);
-  //   console.log("\nsearch for ", text);
-  //   this.browser.sleep(3000);
-  //
-  //   var el = this.browser.element(by.css('form>input.fast-search__input')).sendKeys(text)
-      // .then(function (el) {
-      //   el.sendKeys(text);
-      //
-      // })
-
-      // var search_field = this.browser.findElement({css: '.fast-search__input'})
-      //   .then(function (element) {
-      //     // console.log("FOUND: ", element.getText().then((txt) =>  {
-      //     //   return txt;
-      //     element.sendKeys(text);
-      //     });
-
-        // var css_loc = ".result__item.result__item_product .product__title";
-    //
-    // this.browser.element(by.css(css_loc)).getText()
-    //   .then(function (element) {
-    //     console.log("founded: ", element);
-    //   })
-    // var condition = seleniumWebdriver.until.elementLocated({css: css_loc});
-    // var condition = EC.presenceOf(element(by.css(css_loc)));
-    // return this.browser.wait(condition, 5000);
-  //   return this.browser.sleep(5000);
-  // });
-
-
-  // When('I click phones', function () {
-  //   var css_sel = 'ul.catalog-bar__list a[href="https://catalog.onliner.by/mobile"]';
-  //   var condition = seleniumWebdriver.until.elementLocated({css: css_sel});
-  //   return this.driver.wait(condition, 5000);
-  // });
-
-  // Then('I should see {stringInDoubleQuotes}', function (text) {
-  //   var xpath = "//*[contains(text(),'" + text + "')]";
-  //   var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-  //   return this.driver.wait(condition, 5000);
-  // });
-
-
-
-    // When('I go back', function () {
-    //   this.browser.navigate().back();
-    // })
-
-    //TODO remove
-    // When('I go to the main page', function () {
-    //     // return this.browser.get('https://www.onliner.by');
-    //     var css = 'a[href="https://www.onliner.by/"]';
-    //     return this.browser.$(css).click()
-    //       .then(()=> {
-    //         console.log("CLICKCCKJCKJ");
-    //         this.browser.sleep(10000);
-    //       })
-    // });
-
-    // Then('I should see the title {stringInDoubleQuotes}', function (text) {
-    //   var css_sel = 'title';
-    //       var actualTitle = this.browser.findElement({css: css_sel})
-    //       .then(function (el) {
-    //         el.getText()
-    //           .then(function (txt) {
-    //             console.log("title: ", txt);
-    //             return (txt  === text);
-    //           })
-    //       })
-    // });
-
-
 
 });
